0ac36ea9278b59dc395a1d0a139364c0915220da
Artyom <Artyom@example.com>
2021-01-25 19:13:08 +0600

    add image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import image from '@/images/icon.png'
 
 const IndexPage = (): JSX.Element => {
   return (
     <>
       <h1>HELLO WORLD</h1>
+      <img src={image} alt="no image"/>
     </>
   )
 }
