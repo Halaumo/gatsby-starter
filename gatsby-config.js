9513f0f57e1eadafc4c4ba22d4b03277e6d3fe5b
Artyom <Artyom@example.com>
2021-03-02 11:37:45 +0600

    add gatsby-plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    'gatsby-plugin-sitemap',
     // if in folder has no image remove gatsby-source-filesystem
     {
       resolve: `gatsby-source-filesystem`,
